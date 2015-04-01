158098d02c10cb3ac9018aa80e683beccfc8d074
Michael Williams <m.t.williams@live.com>
2015-04-01 12:16:43 -0700

    Bind Puma to '0.0.0.0:80'.

--- app.sh
+++ app.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /app
+# TODO(mtwilliams): Pre-install dependencies during build?
 # TODO(mtwilliams): Handle production by install Postgres.
 bundle install --without production
-exec bundle exec rackup -p 80 -P /var/run/app.pid >>/var/log/app.log 2>&1
+exec bundle exec rackup -o 0.0.0.0 -p 80 -P /var/run/app.pid >>/var/log/app.log 2>&1
